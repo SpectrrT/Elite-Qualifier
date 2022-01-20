09123e6d2faf6fdddf12c5f789cc3b52d24bdf67
Tensae Laki <97065477+SpectrrT@users.noreply.github.com>
2022-01-20 00:00:12 +0000

    added tests to test.py, added yaml, and added test to yml

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import unittest
 import main
 
+class TestLoad_Words(unittest.TestCase):
+  
+    def test_load_Words(self):
+      self.assertTrue(main.load_words())
 
-
+class TestSuggest(unittest.TestCase):
+  
+    def test_suggest(self):
+      self.assertTrue(main.suggest)     
